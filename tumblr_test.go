66d092168282dfa5e04b253254f109de4bbf4095
Matthew Cunningham <matthew@apcera.com>
2015-07-13 09:58:34 -0700

    testing this commit

--- tumblr_test.go
+++ tumblr_test.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"reflect"
 	"testing"
+	"fmt"
 )
 
 var testClient *Tumblr
@@ -28,6 +29,7 @@ func setup() {
 func TestNew(t *testing.T) {
 	var credentials map[string]string
 	credentialsFile, err := ioutil.ReadFile("./credentials_test.json")
+	fmt.Println(len(credentialsFile))
 	if err != nil {
 		t.Errorf("error opening the credentials file: %s\n", err)
 	}
